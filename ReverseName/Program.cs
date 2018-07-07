31ddcc3ec2a54956e1356829a74acc902df07956
Caleb Quinn <calebq2010@gmail.com>
2018-07-07 14:18:38 -0400

    Completed first program to reverse name need to refactor

--- ReverseName/Program.cs
+++ ReverseName/Program.cs
@@ -10,6 +10,13 @@ namespace ReverseName
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your name:");
+            var name = Console.ReadLine();
+            var nameArray = name.ToCharArray().Reverse();
+            var finalString = string.Join("", nameArray);
+
+            Console.WriteLine("Your name reversed is: " + finalString);
+
         }
     }
 }
